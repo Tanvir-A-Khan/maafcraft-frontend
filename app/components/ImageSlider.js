f3fc9a0081d3615c060ba477753d068a9998007f
Tanvir Ahmed Khan <tanvir@kaz-software.com>
2024-07-08 14:57:57 +0600

    "done"

--- app/components/ImageSlider.js
+++ app/components/ImageSlider.js
@@ -10,6 +10,7 @@ const ImageSlider = () => {
 
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [scroll, setScroll] = useState(5);
 
 
   
@@ -22,30 +23,8 @@ const ImageSlider = () => {
       setLoading(false);
     };
     getData();
-    const startSlider = () => {
-      intervalRef.current = setInterval(() => {
-        if (sliderRef.current) {
-          const maxScrollLeft = sliderRef.current.scrollWidth - sliderRef.current.clientWidth;
-          if (sliderRef.current.scrollLeft >= maxScrollLeft) {
-            sliderRef.current.scrollLeft = 0;
-          } else {
-            sliderRef.current.scrollLeft += 1; // Adjust this value to control the speed
-          }
-        }
-      }, 10); // Adjust the interval time to control the speed
-    };
+   
     
-    startSlider();
-    
-    // const slider = sliderRef.current;
-    // slider.addEventListener('mouseenter', () => clearInterval(intervalRef.current));
-    // slider.addEventListener('mouseleave', startSlider);
-    
-    // return () => {
-    //   clearInterval(intervalRef.current);
-    //   slider.removeEventListener('mouseenter', () => clearInterval(intervalRef.current));
-    //   slider.removeEventListener('mouseleave', startSlider);
-    // };
   }, []);
   
   if (loading || images == null) {
@@ -53,7 +32,7 @@ const ImageSlider = () => {
   }
   return (
     <div className="my-2 overflow-hidden whitespace-nowrap">
-      <marquee>
+      <marquee scrollamount={scroll}  onMouseEnter={()=>setScroll(0)} onMouseLeave={()=>setScroll(5)}>
       <div
         ref={sliderRef}
         className="flex items-center space-x-4 transition-transform duration-300 ease-linear"
