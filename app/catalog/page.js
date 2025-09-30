99259514c5440ede4b33d788f96fe95351c2505f
Tanvir Ahmed Khan <tkhan@cognitus.com>
2025-09-30 15:18:31 +0600

    Updated catalog links and titles for 2022, 2023, 2024, and 2025 Maafcraft Catalogs; removed outdated 2023 link.

--- app/catalog/page.js
+++ app/catalog/page.js
@@ -8,36 +8,28 @@ const Catalog = () => {
             </h1>
             <div className="flex flex-col items-start px-4 py-6 mx-auto mb-8 rounded shadow-md md:px-10 md:py-8 lg:px-20 lg:py-10 bg-slate-100">
                 <a
-                    href="https://barc.portal.gov.bd/sites/default/files/files/barc.portal.gov.bd/page/f152389f_b9fa_4fa6_8cc2_148df01aed6d/2023-06-25-07-23-97982a5e7178e796cf550f598b845147.pdf"
+                    href="https://api.cloudinary.com/v1_1/dsax7rav6/image/download?api_key=172153882346336&attachment=true&audit_context=eyJhY3Rvcl90eXBlIjoidXNlciIsImFjdG9yX2lkIjoiNDU1MTM0NzAyZDdiMTJlM2NmYzZhZjE0NDgwMzZkOGIiLCJ1c2VyX2V4dGVybmFsX2lkIjoiNTg3YjQ1YzNjZTBhZjczYmY0MTc0MWVjODM5OGQ5IiwidXNlcl9jdXN0b21faWQiOiJtYWFmY3JhZnRiZEBnbWFpbC5jb20iLCJjb21wb25lbnQiOiJjb25zb2xlIn0%3D&public_id=Maafcraft_Catalog_2025_gvufat&signature=a334db1a27f72eb06b2ead689269e7d5c95eb4cf&source=ml&target_filename=Maafcraft_Catalog_2025_gvufat&timestamp=1759223569&type=upload"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="mb-2 text-sm md:text-base lg:text-lg hover:font-bold hover:text-green-700"
                 >
-                    📃 2020 Maafcraft Catalog
+                    📃 2025 Maafcraft Catalog
                 </a>
                 <a
-                    href="https://barc.portal.gov.bd/sites/default/files/files/barc.portal.gov.bd/page/f152389f_b9fa_4fa6_8cc2_148df01aed6d/2023-06-25-07-23-97982a5e7178e796cf550f598b845147.pdf"
+                    href="https://api.cloudinary.com/v1_1/dsax7rav6/image/download?api_key=172153882346336&attachment=true&audit_context=eyJhY3Rvcl90eXBlIjoidXNlciIsImFjdG9yX2lkIjoiNDU1MTM0NzAyZDdiMTJlM2NmYzZhZjE0NDgwMzZkOGIiLCJ1c2VyX2V4dGVybmFsX2lkIjoiNTg3YjQ1YzNjZTBhZjczYmY0MTc0MWVjODM5OGQ5IiwidXNlcl9jdXN0b21faWQiOiJtYWFmY3JhZnRiZEBnbWFpbC5jb20iLCJjb21wb25lbnQiOiJjb25zb2xlIn0%3D&public_id=Maafcraft_Catalog_2024_qb89qw&signature=96ae5ed2a7667b9ee6af7cdf5705f3d058c17d9a&source=ml&target_filename=Maafcraft_Catalog_2024_qb89qw&timestamp=1759223695&type=upload"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="mb-2 text-sm md:text-base lg:text-lg hover:font-bold hover:text-green-700"
                 >
-                    📃 2021 Maafcraft Catalog PDF
+                    📃 2024 Maafcraft Catalog PDF
                 </a>
                 <a
-                    href="https://barc.portal.gov.bd/sites/default/files/files/barc.portal.gov.bd/page/f152389f_b9fa_4fa6_8cc2_148df01aed6d/2023-06-25-07-23-97982a5e7178e796cf550f598b845147.pdf"
+                    href="https://api.cloudinary.com/v1_1/dsax7rav6/image/download?api_key=172153882346336&attachment=true&audit_context=eyJhY3Rvcl90eXBlIjoidXNlciIsImFjdG9yX2lkIjoiNDU1MTM0NzAyZDdiMTJlM2NmYzZhZjE0NDgwMzZkOGIiLCJ1c2VyX2V4dGVybmFsX2lkIjoiNTg3YjQ1YzNjZTBhZjczYmY0MTc0MWVjODM5OGQ5IiwidXNlcl9jdXN0b21faWQiOiJtYWFmY3JhZnRiZEBnbWFpbC5jb20iLCJjb21wb25lbnQiOiJjb25zb2xlIn0%3D&public_id=Maafcraft_Catalog_2022_xoxnrw&signature=9dfe388c84bf1ee3be67f8e4fd69a503a5312cc1&source=ml&target_filename=Maafcraft_Catalog_2022_xoxnrw&timestamp=1759223735&type=upload"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="mb-2 text-sm md:text-base lg:text-lg hover:font-bold hover:text-green-700"
                 >
                     📃 2022 Maafcraft Catalog PDF
-                </a>
-                <a
-                    href="https://barc.portal.gov.bd/sites/default/files/files/barc.portal.gov.bd/page/f152389f_b9fa_4fa6_8cc2_148df01aed6d/2023-06-25-07-23-97982a5e7178e796cf550f598b845147.pdf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-sm md:text-base lg:text-lg hover:font-bold hover:text-green-700"
-                >
-                    📃 2023 Maafcraft Catalog* Latest PDF
                 </a>
             </div>
         </div>
